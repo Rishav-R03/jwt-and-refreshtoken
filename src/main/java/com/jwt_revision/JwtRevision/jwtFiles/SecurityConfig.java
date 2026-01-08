b121e10f8bf8b2577d1d7e2cd0926b0fbba60aa0
Rishav-R03 <rishav042023@gmail.com>
2026-01-08 15:33:24 +0530

    Added cors in filterchain and added configuration source generator bean

--- src/main/java/com/jwt_revision/JwtRevision/jwtFiles/SecurityConfig.java
+++ src/main/java/com/jwt_revision/JwtRevision/jwtFiles/SecurityConfig.java
@@ -7,12 +7,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -27,13 +33,14 @@ public class SecurityConfig {
         public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
                 httpSecurity
+                                .cors(Customizer.withDefaults())
                                 .csrf(csrf -> csrf.disable())
                                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                                 .exceptionHandling(ex -> ex
                                                 .authenticationEntryPoint(authenticationEntryPoint)
                                                 .accessDeniedHandler(accessDeniedHandler))
                                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
-                                                .requestMatchers("/public/**").permitAll()
+                                                .requestMatchers("/api/public/**").permitAll()
                                                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
                                                 .requestMatchers("/api/user/**").hasAnyRole("USER", "ADMIN")
                                                 .anyRequest().authenticated())
@@ -47,5 +54,18 @@ public class SecurityConfig {
         public AuthenticationManager authenticationManager(
                         AuthenticationConfiguration config) throws Exception {
                 return config.getAuthenticationManager();
+        }
+
+        //Define a CORS filter Bean to explicitly
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource(){
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(Arrays.asList("*"));
+                configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+                configuration.setAllowedHeaders(Arrays.asList("Authorization","Content-Type","Cache-Control"));
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**",configuration);
+                return source;
         }
 }
