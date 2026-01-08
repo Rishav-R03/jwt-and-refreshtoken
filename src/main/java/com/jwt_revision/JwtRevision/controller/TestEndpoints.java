d84409fbcb5d307d3fe489c9fc22e8ba06c9272e
Rishav-R03 <rishav042023@gmail.com>
2026-01-08 15:10:38 +0530

    added cross origin settings

--- src/main/java/com/jwt_revision/JwtRevision/controller/TestEndpoints.java
+++ src/main/java/com/jwt_revision/JwtRevision/controller/TestEndpoints.java
@@ -2,6 +2,7 @@ package com.jwt_revision.JwtRevision.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class TestEndpoints {
     @GetMapping("/user/dashboard")
     @PreAuthorize("hasRole('USER')")
